051f1e29888df58b0412f3f34d2593231901302b
kemalcanbora <kemalcanbora@gmail.com>
2018-03-29 21:12:17 +0300

    setup py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 setup(
     name='zemberek_parser',
     version='1.0.0',
-    packages=['fonksiyonlar'],
+    packages=['fonksiyonlar','zemberek_connection'],
     url='https://github.com/kemalcanbora/zemberek_parser',
     license='MIT',
     author='Kemalcan Bora',
     author_email='kemalcanbora@gmail.com',
-    description='zemberek kütüphanesinin lite python versiyonu.'
+    description='zemberek kutuphanesinin lite python versiyonu.'
 )
