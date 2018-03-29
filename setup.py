d153ff6bf0ce38bb2137728bbf854c7ca8c91611
kemalcanbora <kemalcanbora@gmail.com>
2018-03-29 21:10:14 +0300

    nltk ayrı class

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='zemberek_parser',
+    version='1.0.0',
+    packages=['fonksiyonlar'],
+    url='https://github.com/kemalcanbora/zemberek_parser',
+    license='MIT',
+    author='Kemalcan Bora',
+    author_email='kemalcanbora@gmail.com',
+    description='zemberek kütüphanesinin lite python versiyonu.'
+)
