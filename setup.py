bf8a321aa4efe1cab9f6b1110d571efbe7f908db
kemalcanbora <kemalcanbora@gmail.com>
2018-03-29 21:18:16 +0300

    fix

--- setup.py
+++ setup.py
@@ -5,8 +5,11 @@ setup(
     version='1.0.0',
     packages=['fonksiyonlar','zemberek_connection'],
     url='https://github.com/kemalcanbora/zemberek_parser',
-    license='MIT',
+    license='BSD',
     author='Kemalcan Bora',
     author_email='kemalcanbora@gmail.com',
-    description='zemberek kutuphanesinin lite python versiyonu.'
+    description='zemberek kutuphanesinin lite python versiyonu',
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+    ]
 )
