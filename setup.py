655d49f61cdf2f875073dcc21673a6c3e109f756
kemalcanbora <kemalcanbora@gmail.com>
2018-03-30 11:52:20 +0300

    v2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='zemberek_parser',
     version='1.0.0',
     packages=['zemberek_python'],
-    ext_modules=[Extension('zemberek-tum-2.0.jar', ['zemberek-tum-2.0.jar'])],
+    package_data ={'zemberek-tum-2.0.jar'},
     url='https://github.com/kemalcanbora/zemberek_parser',
     license='BSD',
     author='Kemalcan Bora',
