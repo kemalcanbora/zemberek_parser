ef94d87de50590a32cb39c576634e73d8d59ec01
kemalcanbora <kemalcanbora@gmail.com>
2018-03-30 11:31:16 +0300

    test

--- zemberek_python/main_libs.py
+++ zemberek_python/main_libs.py
@@ -3,11 +3,11 @@ from collections import Counter
 import snowballstemmer
 from nltk import download
 from nltk.corpus import stopwords
-from .zemberek_connection import zemberek
-
+from zemberek_connection import zemberek
+import os
 
 libjvmpath = "/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/amd64/server/libjvm.so"
-zemberekJarpath = "./zemberek_python/zemberek-tum-2.0.jar"
+zemberekJarpath = "./zemberek-tum-2.0.jar"
 
 zemberek_api = zemberek(libjvmpath, zemberekJarpath)
 
