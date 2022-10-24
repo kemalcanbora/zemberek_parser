486a50ab81f7be7c9c4c2fb867b886d394036a5f
kemalcan bora <kemalcanbora@gmail.com>
2022-10-24 23:31:32 +0200

    setup version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("requirements.txt") as f:
 
 setup(
     name='zemberek_parser',
-    version='1.1.1',
+    version='1.1.2',
     packages=find_packages(),
     url='https://github.com/kemalcanbora/zemberek_parser',
     license='BSD',
