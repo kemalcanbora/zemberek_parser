b32d0d3f2a13efcdc8039475ae9e3711fb877007
kemalcan bora <kemalcanbora@gmail.com>
2022-10-24 22:26:26 +0200

    added MorphologicalAnalysis

--- zemberek_python/settings.py
+++ zemberek_python/settings.py
@@ -1,3 +1,5 @@
+import os
+
 libjvmpath = "/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/amd64/server/libjvm.so"
-zemberekJarpath = "./zemberek_python/zemberek-tum-2.0.jar"
+zemberekJarpath = os.path.dirname(__file__)+"/zemberek-tum-2.0.jar"
 tikaURL = "https://repo1.maven.org/maven2/org/apache/tika/tika-app/1.18/tika-app-1.18.jar"
