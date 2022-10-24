f73ba94c435c9fe6fb4890fdddfc8cca7e363da9
kemalcan bora <kemalcanbora@gmail.com>
2022-10-24 23:26:58 +0200

    fixed setup

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup
+from setuptools import find_packages, setup
 
 try:  # for pip >= 10
     from pip._internal.req import parse_requirements
+
     requirement_field_name = 'requirement'
 except ImportError:  # for pip <= 9.0.3
     requirement_field_name = 'req'
@@ -12,10 +14,10 @@ with open("requirements.txt") as f:
 setup(
     name='zemberek_parser',
     version='1.1.1',
-    packages=['core', 'core.kefir_', 'core.spellChecker', 'core.tika_', 'core.zemberek', 'core.zemberek.parser', 'core.zemberek.parser.tools', 'core.zemberek.parser.tools.ner', 'core.zemberek.parser.tools.pos', 'core.zemberek.parser.tools.stem', 'core.zemberek.parser.tools.tokenization', 'core.zemberek.parser.tools.translation', 'core.zemberek.parser.tools.word', 'core.zemberek.parser.tools.word2vec', 'core.zemberek_python', 'core.zemberek_python.main_libs', 'core.zemberek_python.settings'],
+    packages=find_packages(),
     url='https://github.com/kemalcanbora/zemberek_parser',
     license='BSD',
-    package_data = { '' : ['zemberek-tum-2.0.jar']},
+    package_data={'': ['zemberek-tum-2.0.jar']},
     author='Kemalcan Bora',
     author_email='kemalcanbora@gmail.com',
     description='zemberek kutuphanesinin lite python versiyonu',
@@ -23,6 +25,5 @@ setup(
         "License :: OSI Approved :: BSD License",
     ],
     install_requires=dependencies
-
 
 )
