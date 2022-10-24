be51bac20d5b965c52851add2d90df1f96e74a01
kemalcan bora <kemalcanbora@gmail.com>
2022-10-24 23:25:07 +0200

    init problem fixed #3

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ try:  # for pip >= 10
     from pip._internal.req import parse_requirements
     requirement_field_name = 'requirement'
 except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements
     requirement_field_name = 'req'
 
 with open("requirements.txt") as f:
@@ -13,7 +12,7 @@ with open("requirements.txt") as f:
 setup(
     name='zemberek_parser',
     version='1.1.1',
-    packages=['core', 'core.kefir_', 'core.spellChecker', 'core.tika_', 'core.zemberek', 'core.*'],
+    packages=['core', 'core.kefir_', 'core.spellChecker', 'core.tika_', 'core.zemberek', 'core.zemberek.parser', 'core.zemberek.parser.tools', 'core.zemberek.parser.tools.ner', 'core.zemberek.parser.tools.pos', 'core.zemberek.parser.tools.stem', 'core.zemberek.parser.tools.tokenization', 'core.zemberek.parser.tools.translation', 'core.zemberek.parser.tools.word', 'core.zemberek.parser.tools.word2vec', 'core.zemberek_python', 'core.zemberek_python.main_libs', 'core.zemberek_python.settings'],
     url='https://github.com/kemalcanbora/zemberek_parser',
     license='BSD',
     package_data = { '' : ['zemberek-tum-2.0.jar']},
